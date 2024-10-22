df3e0664e88cc0c3ab113517d68500793d200510
Aleksei5972 <sebastian.trujillo5972@alumnos.udg.mx>
2024-10-22 17:52:34 -0600

    Agregada una carpeta

---
+++ Formularios/formulario.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+  <title> Empleados </title>
+</head>
+
+<body>
+<form action="resultados.php" method="post">
+
+
+<br>Nombre<br>
+<input type="text" name="name"> <br>
+
+<br>Nombre<br>
+<input type="text" name="name"> <br>
+
+<br>Nombre<br>
+<input type="text" name="name"> <br>
+
+<br>Nombre<br>
+<input type="date" name="name"> <br>
+
+
+<input type="submit" value="Enviar"> <input type="reset" value="Borrar">
+
+</form>
+</body>
+</html>
---
+++ Formularios/resultados.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+Entonces usted termino  <?php echo $_POST["Edu"]; ?> <br>
+Aspira a ser <?php echo $_POST["Puesto"]; ?> <br>
+Considera su experiencia laboral como de categoria  <?php echo $_POST["Exp"]; ?> <br>
+Usted se describe como <?php echo $_POST["Desc"]; ?> <br>
+</body>
+</html>
