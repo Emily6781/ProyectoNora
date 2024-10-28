2125edfe301425badabf4f8dbeba01e1da8f1984
Aleksei5972 <sebastian.trujillo5972@alumnos.udg.mx>
2024-10-28 11:27:24 -0600

    Cambio a resultados de empleado
    Creo que luego pondre todo dentro del mismo archivo

--- Formularios/resultados.php
+++ Formularios/resultados.php
@@ -1,8 +1,6 @@
 <html>
 <body>
-Entonces usted termino  <?php echo $_POST["name"]; ?> <br>
-Aspira a ser <?php echo $_POST["ApeP"]; ?> <br>
-Considera su experiencia laboral como de categoria  <?php echo $_POST["ApeM"]; ?> <br>
-Usted se describe como <?php echo $_POST["FechaNac"]; ?> <br>
+Usted es  <?php echo $_POST["name"];?> <?php echo $_POST["ApeP"];?> <?php echo $_POST["ApeM"];?> <br>
+Nacio en el <?php echo $_POST["FechaNac"]; ?> <br>
 </body>
 </html>
